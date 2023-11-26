ad39458261de80ade4e9987869570c8b0057f746
joshua-kannan <joshuakannanwork@gmail.com>
2023-11-26 20:39:24 +0530

    type

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -21,7 +21,7 @@ export default function Navbar() {
       <div className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
         <Link href="/">
           <h1 className="text-2xl md:text-4xl font-bold">
-            JK <span className="text-primary">Watch Store</span>
+            JK <span className="text-primary">Watch Shop</span>
           </h1>
         </Link>
 
